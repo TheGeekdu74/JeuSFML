438079c4cb3d79421787f513666a51f596da4969
Alexandre VUILLEROT <vuillera@iut-acy.local>
2018-11-16 12:51:48 +0100

    Update sf

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    // Create the main window
-    sf::RenderWindow app(sf::VideoMode(800, 600), "SFML window");
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-
-	// Start the game loop
+    RenderWindow app(VideoMode(800, 600), "SFML window");
     while (app.isOpen())
     {
-        // Process events
-        sf::Event event;
+        Event event;
         while (app.pollEvent(event))
         {
-            // Close window : exit
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 app.close();
         }
-
-        // Clear screen
-        app.clear();
-
-        // Draw the sprite
-        app.draw(sprite);
-
-        // Update the window
-        app.display();
     }
 
     return EXIT_SUCCESS;
