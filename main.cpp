a58d55a0d58491a29995e33faa51096198859805
Alexandre VUILLEROT <vuillera@iut-acy.local>
2018-11-19 11:17:13 +0100

    Setup moving

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <SFML/Network.hpp>
 #include <iostream>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 using namespace sf;
 using std::cout;
 
@@ -24,51 +27,81 @@ Packet& operator >>(sf::Packet& packet, Data& data)
 
 int main()
 {
-    RenderWindow app(VideoMode(800, 600), "SFML window");
-    RectangleShape rectangle(Vector2f(200,200));
+    Data data1;
+    data1.x = 25;
+    data1.y = 25;
+    RenderWindow app(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML window");
+    RectangleShape rect(Vector2f(50,50));
+    rect.setOrigin(25,25);
+    rect.setPosition(data1.x, data1.y);
 
     while (app.isOpen())
     {
-        UdpSocket socket;
-        socket.setBlocking(false);
-        unsigned short port = 80;
+//        UdpSocket socket;
+//
+//        socket.setBlocking(false);
+//        unsigned short port = 80;
+//
+//        Packet player1;
+//        Packet player2;
+//        Data data;
+//
+//        data.x = 10;
+//        data.y = 20;
+//        data.hp = 100;
+//
+//        player1 << data;
+//
+//        if(socket.bind(port) != Socket::Done){}
+//
+//        IpAddress recipient = "127.0.0.1";
+//        if(socket.send(player1, recipient, port) != Socket::Done){}
+//
+//        IpAddress sender;
+//        if(socket.receive(player2, sender, port) != Socket::Done){}
+//
+//        Data data2;
+//        player2 >> data;
+//        cout << data2.hp << std::endl;
 
-        Packet player1;
-        Packet player2;
-        Data data;
 
-        data.x = 10;
-        data.y = 20;
-        data.hp = 100;
-
-        player1 << data;
-
-        // lie la socket à un port
-        if(socket.bind(port) != Socket::Done)
-            printf("Erreur de connexion au port");
-
-        IpAddress recipient = "127.0.0.1";
-
-        if(socket.send(player1, recipient, port) != Socket::Done)
-            printf("Erreur d'envoi");
-
-        IpAddress sender;
-
-        if(socket.receive(player2, sender, port) != Socket::Done)
-            printf("Erreur de reception");
-
-        Data data2;
-        player2 >> data;
-        cout << data2.hp << std::endl;
-
+        if(Keyboard::isKeyPressed(Keyboard::Z)){
+            if(data1.y >= 25){
+                data1.y -= 1;
+                rect.setPosition(data1.x, data1.y);
+            }
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Q)){
+            if(data1.x >= 25){
+               data1.x -= 1;
+                rect.setPosition(data1.x, data1.y);
+            }
+        }
+        if(Keyboard::isKeyPressed(Keyboard::S)){
+            if(data1.y + 25 <= WINDOW_HEIGHT){
+                data1.y += 1;
+                rect.setPosition(data1.x, data1.y);
+            }
+        }
+        if(Keyboard::isKeyPressed(Keyboard::D)){
+            if(data1.x + 25 <= WINDOW_WIDTH){
+               data1.x += 1;
+                rect.setPosition(data1.x, data1.y);
+            }
+        }
 
         Event event;
         while (app.pollEvent(event)) {
-            if (event.type == Event::Closed)
-                app.close();
+            switch(event.type){
+                case Event::Closed:
+                    app.close();
+                    break;
+            }
         }
         app.clear();
+        app.draw(rect);
         app.display();
+
     }
     return 0;
 }
