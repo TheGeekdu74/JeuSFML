1fb8c8bcf078712a74eb52c33d1f679d80f62e84
Alexandre VUILLEROT <vuillera@iut-acy.local>
2018-11-19 12:16:30 +0100

    Updated jump

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 #include <iostream>
+#include <unistd.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
@@ -29,11 +30,13 @@ int main()
 {
     Data data1;
     data1.x = 25;
-    data1.y = 25;
+    data1.y = WINDOW_HEIGHT - 25;
     RenderWindow app(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML window");
     RectangleShape rect(Vector2f(50,50));
     rect.setOrigin(25,25);
     rect.setPosition(data1.x, data1.y);
+
+    //------------- LIFE BARS SETUP --------------
 
     RectangleShape lifeBar1(Vector2f(200, 50));
     lifeBar1.setFillColor(Color::Red);
@@ -83,57 +86,62 @@ int main()
 //        player2 >> data;
 //        cout << data2.hp << std::endl;
 
-        if(Keyboard::isKeyPressed(Keyboard::Z)){
-            /*if(data1.y >= 25){
+        if(Keyboard::isKeyPressed(Keyboard::Z)){ //Montee jump
+            while(data1.y > 200){
                 data1.y -= 1;
-                rect.setPosition(data1.x, data1.y);
-            }*/
-            while(data1.y>200){
-                data1.y -= 1;
-                usleep(0.1);
+                usleep(0.5);
                 rect.setPosition(data1.x,data1.y);
+
                 if(Keyboard::isKeyPressed(Keyboard::Q)){
-            if(data1.x >= 25){
-               data1.x -= 1;
-                rect.setPosition(data1.x, data1.y);
-            }
+                    if(data1.x >= 25){
+                        data1.x -= 1;
+                        rect.setPosition(data1.x, data1.y);
+                    }
                 }
-            if(Keyboard::isKeyPressed(Keyboard::D)){
-            if(data1.x + 25 <= WINDOW_WIDTH){
-               data1.x += 1;
-                rect.setPosition(data1.x, data1.y);
-            }
-            }
+                if(Keyboard::isKeyPressed(Keyboard::D)){
+                    if(data1.x + 25 <= WINDOW_WIDTH){
+                        data1.x += 1;
+                        rect.setPosition(data1.x, data1.y);
+                    }
+                }
+
                 app.clear();
                 app.draw(rect);
+                app.draw(lifeBar1);
+                app.draw(lifeBarBorder1);
+                app.draw(lifeBarBorder2);
+                app.draw(lifeBar2);
                 app.display();
-
             }
-            while(data1.y + 25 < WINDOW_HEIGHT){
+            while(data1.y + 25 < WINDOW_HEIGHT){ //Descente jump
                 data1.y += 1;
-                usleep(0.1);
+                usleep(0.5);
                 rect.setPosition(data1.x,data1.y);
                 if(Keyboard::isKeyPressed(Keyboard::Q)){
-            if(data1.x >= 25){
-               data1.x -= 1;
-                rect.setPosition(data1.x, data1.y);
-            }
+                    if(data1.x >= 25){
+                        data1.x -= 1;
+                        rect.setPosition(data1.x, data1.y);
+                    }
                 }
             if(Keyboard::isKeyPressed(Keyboard::D)){
-            if(data1.x + 25 <= WINDOW_WIDTH){
-               data1.x += 1;
-                rect.setPosition(data1.x, data1.y);
-            }
+                if(data1.x + 25 <= WINDOW_WIDTH){
+                    data1.x += 1;
+                    rect.setPosition(data1.x, data1.y);
+                }
             }
                 app.clear();
                 app.draw(rect);
+                app.draw(lifeBar1);
+                app.draw(lifeBarBorder1);
+                app.draw(lifeBarBorder2);
+                app.draw(lifeBar2);
                 app.display();
-
             }
         }
         if(Keyboard::isKeyPressed(Keyboard::Q)){
             if(data1.x >= 25){
-               data1.x -= 1;
+                data1.x -= 1;
+                usleep(0.5);
                 rect.setPosition(data1.x, data1.y);
             }
         }
@@ -145,7 +153,8 @@ int main()
         }
         if(Keyboard::isKeyPressed(Keyboard::D)){
             if(data1.x + 25 <= WINDOW_WIDTH){
-               data1.x += 1;
+                data1.x += 1;
+                usleep(0.5);
                 rect.setPosition(data1.x, data1.y);
             }
         }
